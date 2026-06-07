d9eec8346950331b10f05a3fd417a756d376d52a
AleXRD <alesgxrd@gmail.com>
2026-06-07 11:38:57 -0300

    add a find method for pulses sent

--- feelink-backend/src/main/java/com/sigith/feelink/repository/IPulseRepository.java
+++ feelink-backend/src/main/java/com/sigith/feelink/repository/IPulseRepository.java
@@ -1,9 +1,28 @@
 package com.sigith.feelink.repository;
 
 import com.sigith.feelink.model.Pulse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IPulseRepository extends JpaRepository<Pulse, String> {
+    @Query("""
+        SELECT p
+        FROM Pulse p
+        WHERE p.fromUser.id = :fromUserId
+        AND (
+            :toUserId IS NULL
+            OR p.toUser.id = :toUserId
+        )
+    """)
+    Page<Pulse> findSentPulses(
+            String fromUserId,
+            String toUserId,
+            Pageable pageable
+    );
 }
--- feelink-backend/src/main/java/com/sigith/feelink/service/PulseService.java
+++ feelink-backend/src/main/java/com/sigith/feelink/service/PulseService.java
@@ -13,7 +13,14 @@ import com.sigith.feelink.repository.IMessageRepository;
 import com.sigith.feelink.repository.IPulseRepository;
 import com.sigith.feelink.repository.IUserRepository;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -60,5 +67,23 @@ public class PulseService {
         Pulse saved = pulseRepository.save(pulse);
 
         return pulseMapper.toResponseDto(saved);
+    }
+
+    public Page<ResponsePulseDTO> getSentPulses(
+            String fromUserId,
+            String toUserId,
+            int page,
+            int size
+    ){
+        Pageable pageable = PageRequest.of(
+                page,
+                size,
+                Sort.by("createdAt").descending()
+        );
+
+        return  pulseRepository.findSentPulses(fromUserId, toUserId, pageable)
+                .stream()
+                .map(pulseMapper::toResponseDto)
+                .toList();
     }
 }
